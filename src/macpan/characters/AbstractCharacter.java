806db2a8f8e38359223c018ccceb62920dc2170b
nobur7839 <nobur7839@BDYX082.Student.UGDSB.ED>
2022-12-20 13:40:06 -0500

    made classes x2

--- src/macpan/characters/AbstractCharacter.java
+++ src/macpan/characters/AbstractCharacter.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package macpan.objects;
+package macpan.characters;
 
 /**
  *
  * @author nobur7839
  */
-abstract class  AbstractConsumable {
+abstract class AbstractCharacter {
     
 }
