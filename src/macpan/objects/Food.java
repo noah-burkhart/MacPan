b0001974fc75735b3dc65c8d53c2034164387b43
Noah Burkhart <noahburkhart@hotmail.com>
2022-12-22 11:52:01 -0500

    finished food and pellet classes

--- src/macpan/objects/Food.java
+++ src/macpan/objects/Food.java
@@ -1,13 +1,65 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+ * N Burkhart
+ * 12/22/2022
+ * Object made to represent food in the game
+*/
 package macpan.objects;
 
-/**
- *
- * @author nobur7839
- */
-public class Food {
+import java.awt.Image;
+
+
+public class Food extends AbstractConsumable{
+    
+    /**
+     * General constructor for food items.
+     * @param x - the x position
+     * @param y - the y position
+     */
+    public Food(int x, int y){
+        super(x, y);
+        //sprite = foodImg;
+        points = 50; //assigns the point value for the food
+    }
+    
+    /**
+     * Constructor for food with described variables
+     * @param sprite - the sprite
+     * @param x - the x position
+     * @param y - the y position
+     * @param points - the points it is worth
+     */
+    public Food(Image sprite, int x, int y, int points){
+        super(sprite, x, y, points);
+    }
+    
+    /**
+     * Clones the food object ignoring location
+     * @return - the clone
+     */
+    public Food clone(){
+        Food o = new Food(0, 0);
+        o.setPoints(points);  //clones points and sprite.
+        o.setSprite(sprite);
+        o.visible = true; //makes it visible by default
+        return o; //the new food
+    }
+    
+    /**
+     * Will see if the food is equal to the other in terms of its sprite and its point value.
+     * @param o - the other food object
+     * @return - if they are equal
+     */
+    public boolean equals(Food o){
+        return super.equals(o);
+    }
+
+    @Override
+    /**
+     * Puts the attributes into a string
+     */
+    public String toString() {
+        return "Food{" + super.toString() + '}';
+    }
+    
     
 }
