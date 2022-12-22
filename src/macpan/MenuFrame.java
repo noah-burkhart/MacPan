901d08280e49e145ee46f211b39dbec8ee8b2096
Noah Burkhart <noahburkhart@hotmail.com>
2022-12-22 11:12:20 -0500

    added image to files

--- src/macpan/MenuFrame.java
+++ src/macpan/MenuFrame.java
@@ -34,11 +34,11 @@ public class MenuFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 396, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 278, Short.MAX_VALUE)
         );
 
         pack();
