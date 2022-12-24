6cc8bda2723ca7e30b4260dd323b55c666c1f36f
jel3 <jel3@10.16.22.144>
2022-12-23 19:31:03 -0500

    Revert "Revert "Revert "Noahs changes committed"""
    This reverts commit 2f157743702f30a5bf510ff1f16ecb25cdd0ea57.

--- src/macpan/objects/PowerPellet.java
+++ src/macpan/objects/PowerPellet.java
@@ -4,35 +4,10 @@
  */
 package macpan.objects;
 
-import java.awt.Image;
-
 /**
  *
  * @author nobur7839
  */
 public class PowerPellet extends AbstractConsumable{
     
-    /**
-     * General constructor for PowerPellet items.
-     *
-     * @param x - the x position
-     * @param y - the y position
-     */
-    public PowerPellet(int x, int y) {
-        super(x, y);
-        //sprite = foodImg;
-        points = 50; //assigns the point value for the food
-    }
-
-    /**
-     * Constructor for PowerPellet with described variables
-     *
-     * @param sprite - the sprite
-     * @param x - the x position
-     * @param y - the y position
-     * @param points - the points it is worth
-     */
-    public PowerPellet(Image sprite, int x, int y, int points) {
-        super(sprite, x, y, points);
-    }
 }
