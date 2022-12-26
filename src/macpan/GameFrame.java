c6b0ccb8c1cb21806597f7a9806c63b493bd1050
jel3 <jel3@10.16.22.144>
2022-12-26 00:33:27 -0500

    Drew out the game board with images

--- src/macpan/GameFrame.java
+++ src/macpan/GameFrame.java
@@ -1,13 +1,37 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Jack Luhta
+ * December 25, 2022
+ * 
  */
 package macpan;
 
-/**
- *
- * @author jel3
- */
-public class GameFrame {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class GameFrame extends JFrame {
+
+    MenuFrame mainWindow;
     
+    //constructor
+    public GameFrame(MenuFrame m) {
+        //create the User interface
+        initUI();
+        mainWindow = m;
+    }
+
+    //create the custom JFrame
+    private void initUI() {        
+        //set title of the JFrame
+        setTitle("MACPAN");
+        //add a custom JPanel to draw on
+        add(new GamePanel());
+        //set the size of the window
+        setSize(576, 800);
+        //tell the JFrame what to do when closed
+        //this is important if our application has multiple windows
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
 }
