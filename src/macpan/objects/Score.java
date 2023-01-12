671c38caa6bcb0160496247854d9bf7fe6aba768
vihe8877 <88011859+vihe8877@users.noreply.github.com>
2023-01-12 11:24:43 -0500

    made score an object to be able to hold int and string value

---
+++ src/macpan/objects/Score.java
@@ -0,0 +1,85 @@
+/*
+ * V. He
+ * A Score class to hold the score (int) and the initials of user (string) in one object
+ * Jan 11th, 2023
+ */
+package macpan.objects;
+
+import java.util.Objects;
+
+public class Score {
+    private int value;
+    private String name;
+
+    /**
+     * Primary Constructor for a score
+     * @param value - int value of score
+     * @param name - initials of player (3 characters)
+     */
+    public Score(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    /**
+     * Accessor Method for value
+     * @return - the value of score
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Accessor Method for Name
+     * @return - the initials of player
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Mutator Method for score value
+     * @param value - value of score
+     */
+    public void setvalue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Mutator Method for player name
+     * @param name - player's initials 
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * toString method for Score
+     * @return - all of score's information
+     */
+    public String toString() {
+        return "Score{" + "value=" + value + ", name=" + name + '}';
+    }
+
+    /**
+     * equals method for score
+     * @param obj - another score
+     * @return - true or false if they are equal or not
+     */
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Score other = (Score) obj;
+        if (this.value != other.value) {
+            return false;
+        }
+        return Objects.equals(this.name, other.name);
+    }   
+}
