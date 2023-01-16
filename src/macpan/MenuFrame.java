16d63a0e1c054b5050d25440b46e4fdf6ea605dc
vihe8877 <88011859+vihe8877@users.noreply.github.com>
2023-01-16 11:14:55 -0500

    yessir

--- src/macpan/MenuFrame.java
+++ src/macpan/MenuFrame.java
@@ -9,6 +9,8 @@ import java.awt.Color;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class MenuFrame extends javax.swing.JFrame {
 
